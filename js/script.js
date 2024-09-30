2e79e848b0bd48f1ec8b68972fdf9a6c398429cb
anisenson <adamjn524@gmail.com>
2024-09-30 08:46:21 -0400

    added hover

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(function () {
-   $('.swatch').on('click', function() {
+   $('.swatch').on('mouseenter', function() {
     $('#product-photo').attr('src', 'img/chair-' + $(this).attr('id') + '@2x.jpg');
+    $('.swatch').removeClass('selected');
+    $(this).addClass('selected');
+   
    })
 });
